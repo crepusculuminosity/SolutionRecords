ae342e1518c008354a01351b388ab261d9a9924a
crepusculuminosity <58301704+crepusculuminosity@users.noreply.github.com>
2021-03-26 17:19:13 +0800

    3/26/win

--- nowcoder/exerc/16525.cc
+++ nowcoder/exerc/16525.cc
@@ -1,20 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, m, k, x;
 
-int qsm(int a, int b) {
-  int res = 1;
+typedef long long ll;
+const ll mod = 1e7;
+
+// a ^ b
+ll ksm(ll a, ll b, ll mod) {
+  ll ans = 1, base = a;
   while (b) {
     if (b & 1)
-      res = res * a % n;
-    a = a * a % n;
+      ans = (ans * base) % mod;
+    base = (base * base) % mod;
     b >>= 1;
   }
-  return res % n;
+  return ans;
+}
+
+// a * b
+ll ksc(ll a, ll b, ll mod) {
+  ll ans = 0;
+  while (b) {
+    if (b & 1)
+      ans = (ans + a) % mod;
+    a = (a + a) % mod;
+    b >>= 1;
+  }
+  return ans;
 }
 
 int main() {
-  cin >> n >> m >> k >> x;
-  cout << (x + m * qsm(10, k) % n) % n;
+  cout << "5 ^ 3 = " << ksm(43242424, 3131, mod) << endl;
+  cout << "345352 * 11 = " << ksc(345352, 11, mod) << endl;
+  system("pause");
   return 0;
 }
