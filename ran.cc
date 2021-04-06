f9f3955736c0dcb015736554174a59fe223bebdd
crepusculuminosity <erpangwang111@gmail.com>
2021-04-06 18:03:18 +0800

    4/6/2021

--- ran.cc
+++ ran.cc
@@ -1,7 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+constexpr int maxn=1e8;
 int main() {
-  cout << "here";
+  freopen("data.in","w",stdout);
+  srand((unsigned)time(NULL));
+  for(int i=1;i<=20;i++)
+    cout<<(rand()%maxn)+1<<endl;
+  for(int i=1;i<=100;i++)
+    cout<<(rand()%maxn)+1+maxn<<endl;
   return 0;
 }
