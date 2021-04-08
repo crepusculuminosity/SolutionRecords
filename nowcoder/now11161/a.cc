3ce7e1faa32a9abedfac29d6a7e8899a46568838
crepusculuminosity <erpangwang111@gmail.com>
2021-04-08 20:39:16 +0800

    modified

--- nowcoder/now11161/a.cc
+++ nowcoder/now11161/a.cc
@@ -1,33 +1,51 @@
-#include <bits/stdc++.h>
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#define N 100005
+#define M 50
+
 using namespace std;
-constexpr int maxn = 1e5 + 10;
-int a[maxn], f[maxn], g[maxn];
+
+char a[N], b[M], st[M];
+int ans = 0, n, m, len;
+
+bool bz[N];
+
+void dfs(int x) {
+  if (x > m) {
+    len = 0;
+    for (int i = 1; i <= m; i++)
+      if (bz[i])
+        st[++len] = b[i];
+    for (int i = 1; i <= len / 2; i++)
+      if (st[i] != st[len - i + 1])
+        return;
+    int i = 1, j = 1;
+    for (int i = 1; i <= n; i++) {
+      if (a[i] == st[j])
+        j++;
+      if (j > len)
+        break;
+    }
+    if (j <= len)
+      return;
+    ans = max(ans, len);
+    return;
+  }
+  bz[x] = true;
+  dfs(x + 1);
+  bz[x] = false;
+  dfs(x + 1);
+}
 
 int main() {
-  // freopen("data.in", "r", stdin);
-  // freopen("data.out", "w", stdout);
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr), cout.tie(nullptr);
-  int n;
-  cin >> n;
-  for (int i = 1; i <= n; i++)
-    cin >> a[i];
-  f[1] = g[1] = a[1];
-  f[2] = max(a[1], a[2]), g[2] = min(a[1], a[2]);
-  for (int i = 2; i < n; i++) {
-    if (a[i + 1] >= f[i])
-      f[i + 1] = a[i + 1], g[i + 1] = f[i];
-    else {
-      f[i + 1] = f[i];
-      g[i + 1] = max(a[i + 1], g[i]);
-    }
-  }
-  int q;
-  cin >> q;
-  while (q--) {
-    int x;
-    cin >> x;
-    cout << g[x] << '\n';
-  }
-  return 0;
+  //freopen("data.in", "r", stdin);
+  //freopen("data.out", "w", stdout);
+  scanf("%s", a + 1);
+  n = strlen(a + 1);
+  scanf("%s", b + 1);
+  m = strlen(b + 1);
+  dfs(1);
+  printf("%d", ans);
 }
