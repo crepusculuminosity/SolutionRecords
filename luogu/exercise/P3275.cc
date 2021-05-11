8cce0fd4bf1d440a02f14aea243ede3eafacec67
crepusculuminosity <erpangwang111@gmail.com>
2021-05-11 08:36:35 +0800

    5/11

--- luogu/exercise/P3275.cc
+++ luogu/exercise/P3275.cc
@@ -1,25 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-inline int read() {
-  int x = 0;
-  char ch = getchar();
-  while (ch < '0' || ch > '9')
-    ch = getchar();
-  while (ch >= '0' && ch <= '9')
-    x = (x << 1) + (x << 3) + ch - 48, ch = getchar();
-  return x;
-}
+constexpr int maxn = 100000 + 10;
+constexpr int INF = -0x3f3f3f3f;
 int n, m;
-constexpr int maxn = 1e6 + 10;
 struct node {
   int to, w;
 };
 vector<node> G[maxn];
-int d[maxn], num[maxn], vis[maxn];
 inline void addedge(int u, int v, int w) { G[u].push_back(node{v, w}); }
+int d[maxn], vis[maxn], num[maxn];
 
 bool SPFA(int s) {
-  memset(d, 0x3f, sizeof(d));
+  // memset(d, 0x3f, sizeof(d));
+  for (int i = 1; i <= n; i++)
+    d[i] = INF;
   d[s] = 0;
   vis[s] = 1;
   num[s]++;
@@ -31,7 +25,7 @@ bool SPFA(int s) {
     vis[u] = 0;
     for (int i = 0; i < G[u].size(); i++) {
       node &e = G[u][i];
-      if (d[e.to] > d[u] + e.w) {
+      if (d[e.to] < d[u] + e.w) {
         d[e.to] = d[u] + e.w;
         if (!vis[e.to]) {
           q.push(e.to);
@@ -45,41 +39,45 @@ bool SPFA(int s) {
   }
   return 1;
 }
-
 int main() {
-  // freopen("data.in", "r", stdin);
+
+  // freopen("candy5.in", "r", stdin);
   // freopen("data.out", "w", stdout);
-  n = read(), m = read();
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr), cout.tie(nullptr);
+  cin >> n >> m;
   for (int i = 1; i <= m; i++) {
-    int x = read(), a = read(), b = read();
-    if (x == 1) {
-      addedge(a, b, 0);
-      addedge(b, a, 0);
-    } else if (x == 2) {
+    int op, a, b;
+    cin >> op >> a >> b;
+    if (op == 1)
+      addedge(a, b, 0), addedge(b, a, 0);
+    else if (op == 2) {
       if (a == b) {
         puts("-1");
         return 0;
       }
-      addedge(a, b, -1);
-    } else if (x == 3)
+      addedge(a, b, 1);
+    } else if (op == 3)
       addedge(b, a, 0);
-    else if (x == 4) {
+    else if (op == 4) {
       if (a == b) {
         puts("-1");
         return 0;
       }
-      addedge(b, a, -1);  
-    } else
+      addedge(b, a, 1);
+    }
+
+    else
       addedge(a, b, 0);
   }
   for (int i = 1; i <= n; i++)
-    addedge(0, i, -1);
+    addedge(0, i, 1);
   long long ans = 0;
   if (SPFA(0))
     for (int i = 1; i <= n; i++)
-      ans -= d[i];
+      ans += d[i];
   else
     ans = -1;
-  printf("%lld", ans);
+  cout << ans;
   return 0;
 }
